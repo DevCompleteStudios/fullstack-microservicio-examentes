65836832e56f13da11dbfec78c6660a2e0d1349b
DevCompleteStudios <jorge.yael333@gmail.com>
2025-01-04 17:30:46 -0600

    First commit

---
+++ src/main/java/com/devstudios/microservicio/examenes/microservicio_examenes/MicroservicioExamenesApplication.java
@@ -0,0 +1,13 @@
+package com.devstudios.microservicio.examenes.microservicio_examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioExamenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioExamenesApplication.class, args);
+	}
+
+}
